43920e311fdee21769fc5f060b3c18b3c8dd820e
Luis Nesi <lenesi@hotmail.com>
2017-08-03 12:51:06 +0100

    Play Again

--- src/states/GameOverState.ts
+++ src/states/GameOverState.ts
@@ -7,13 +7,13 @@ class GameOverState extends LoadableState{
 		gameOverText.x=(Game.globalWidth/2)-gameOverText.width/2;
 		this.contentLayer.add(gameOverText);
 
-		let startMessage = new Phaser.BitmapText(this.game,0,Game.globalHeight-200,'PT Mono',"TOUCH TO START",20);
+		let startMessage = new Phaser.BitmapText(this.game,0,Game.globalHeight-200,'PT Mono',"PLAY AGAIN",20);
 		startMessage.x=(Game.globalWidth/2)-startMessage.width/2;
 		let tween:Phaser.Tween=this.game.add.tween(startMessage);
 		tween.to({alpha: 0.2},500,"Linear",true,0,-1,true);
 		this.contentLayer.add(startMessage);
 	}
-	
+
 	update(){
 		if(this.listening){
 			this.capturePointer(this.game.input.mousePointer);
